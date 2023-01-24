fce89aa061c2047f386848799a67d7a0414f7fef
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-24 14:23:18 +0300

    created pip with an example

--- app/src/main/java/com/fcenesiz/floatingwindowsusage/MainActivity.kt
+++ app/src/main/java/com/fcenesiz/floatingwindowsusage/MainActivity.kt
@@ -1,18 +1,50 @@
 package com.fcenesiz.floatingwindowsusage
 
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Bundle
+import android.util.Rational
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toAndroidRect
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.fcenesiz.floatingwindowsusage.ui.theme.FloatingWindowsUsageTheme
 
 class MainActivity : ComponentActivity() {
+
+    class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            println("Clicked on PIP action")
+        }
+    }
+
+    private val isPipSupported by lazy {
+        packageManager.hasSystemFeature(
+            PackageManager.FEATURE_PICTURE_IN_PICTURE
+        )
+    }
+
+    private var videViewBounds = Rect()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,9 +54,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    
+                    AndroidView(
+                        factory = {
+                            VideoView(it, null).apply {
+                                setVideoURI(
+                                    Uri.parse(
+                                        "android.resource://$packageName/${R.raw.sample}"
+                                    )
+                                )
+                                start()
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .onGloballyPositioned {
+                                videViewBounds = it
+                                    .boundsInWindow()
+                                    .toAndroidRect()
+                            }
+                    )
                 }
             }
+        }
+    }
+
+    private fun updatedPipParams(): PictureInPictureParams? {
+        return PictureInPictureParams.Builder()
+            .setSourceRectHint(videViewBounds)
+            .setAspectRatio(Rational(16, 9))
+            .setActions(
+                listOf(
+                    RemoteAction(
+                        Icon.createWithResource(
+                            applicationContext,
+                            R.drawable.ic_android_black_24dp
+                        ),
+                        "Baby changing station",
+                        "Baby changing station",
+                        PendingIntent.getBroadcast(
+                            applicationContext,
+                            0,
+                            Intent(applicationContext, MyReceiver::class.java),
+                            PendingIntent.FLAG_IMMUTABLE
+                        )
+                    )
+                )
+            )
+            .build()
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (!isPipSupported)
+            return
+
+        updatedPipParams()?.let { params ->
+            enterPictureInPictureMode(params)
         }
     }
 }
